7c5be3ed593df823b517e0343863f1674940277a
Matheus Lira <matheustlira@gmail.com>
2021-06-11 13:10:45 -0300

    Post Participants done

--- src/app.js
+++ src/app.js
@@ -5,6 +5,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const participants = [];
+const messages = [];
+
+app.post("/participants", (req, res) => {
+  const participant = req.body;
+  if (
+    participant.name === "" ||
+    participants.find((p) => p.name === participant.name)
+  ) {
+    res.status(400);
+    res.send("Name its blank or there is already a participant with that name");
+  }
+  participant.lastStatus = Date.now();
+  participants.push(participant);
+  res.send(participants);
+});
 app.listen(4000, () => {
   console.log("Server running");
 });
